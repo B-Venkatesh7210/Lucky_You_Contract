7f343876d1607894a4ab3508baf73e4eaa2e1c55
B-Venkatesh7210 <67518179+B-Venkatesh7210@users.noreply.github.com>
2022-03-25 01:29:09 +0530

    Giveaway is working fine

--- LuckyYou.sol
+++ LuckyYou.sol
@@ -8,21 +8,60 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import {
+    SafeERC20,
+    IERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
-contract LuckyYou is VRFConsumerBase
+abstract contract OpsReady {
+    address public immutable ops;
+    address payable public immutable gelato;
+    address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    modifier onlyOps() {
+        require(msg.sender == ops, "OpsReady: onlyOps");
+        _;
+    }
+
+    constructor(address _ops) {
+        ops = _ops;
+        gelato = IOps(_ops).gelato();
+    }
+
+    function _transfer(uint256 _amount) internal {
+        (bool success, ) = gelato.call{value: _amount}("");
+        require(success, "_transfer: ETH transfer failed");
+    }
+}
+
+interface IOps {
+  function createTaskNoPrepayment(
+      address _execAddress,
+      bytes4 _execSelector,
+      address _resolverAddress,
+      bytes calldata _resolverData,
+      address _feeToken
+  ) external returns (bytes32 task);
+
+  function gelato() external view returns (address payable);
+
+  function getFeeDetails() external view returns (uint256, address) ;
+}
+
+
+contract LuckyYou is VRFConsumerBase, OpsReady
 {
 
     address public owner;
-    address public contractAddress;
     using Counters for Counters.Counter;
     Counters.Counter private giveawayNumber;
 
     bytes32 internal keyHash;
     uint256 internal fee;
-    address vrfCoordinator = 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255;
-    address link = 	0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-    uint256 public randomResult; //remove later
+    address vrfCoordinator = 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B;
+    address link = 	0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    address opsAddress = 0x8c089073A9594a4FB03Fa99feee3effF0e2Bc58a; 
     uint public currGiveawayId=0;
     bool public isLocked=false;
 
@@ -31,14 +70,44 @@ contract LuckyYou is VRFConsumerBase
     
 
 
-    constructor() VRFConsumerBase(vrfCoordinator, link) payable {
-        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+    constructor() VRFConsumerBase(vrfCoordinator, link) OpsReady(opsAddress) payable {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
         fee = 0.1 * 10 ** 18; 
-        
         owner = msg.sender;
-        contractAddress = address(this);
-        
-    }
+        }
+
+    event GiveawayCreated(
+        address creator,
+        uint256 uniqueId,
+        string message,
+        string socialLink,
+        uint256 deadline,
+        uint256 timestamp,
+        uint amount,
+        uint participationFee,
+        address[] participants,
+        bool isLive,
+        address winner,
+        bytes32 taskId
+    );
+
+    event GiveawayParticipated(
+        address creator,
+        uint256 uniqueId,
+        bool isLive,
+        uint amount,
+        uint participationFee,
+        address[] participants
+    );
+
+    event GiveawayEnded(
+        address creator,
+        uint256 uniqueId,
+        bool isLive,
+        uint amount,
+        uint participationFee,
+        address winner
+    );
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
@@ -52,53 +121,75 @@ contract LuckyYou is VRFConsumerBase
         address creator;
         uint256 uniqueId;
         string message;
+        string socialLink;
         uint256 deadline;
         uint256 timestamp;
         uint amount;
+        uint participationFee;
         address[] participants;
         bool isLive;
         address winner;
+        bytes32 taskId;
     }
 
     mapping (uint256 => Giveaway) public giveawayMap;
 
-    function createGiveaway(string memory _message, uint256 _deadline) public payable {
+    function createGiveaway(string memory _message, string memory _socialLink, uint256 _deadline) public payable {
 
-        //Add a check for block.timestamp to be less than deadline
+        require(block.timestamp<_deadline, "Enter valid time");
         require(msg.value>0, "No value given");
+        uint _participationFee = msg.value/100;
         address[] memory empty;
         giveawayNumber.increment();
-        
         uint256 newGiveawayNumber = giveawayNumber.current();
-        giveawayMap[newGiveawayNumber] = Giveaway(msg.sender, newGiveawayNumber, _message, _deadline, block.timestamp, msg.value, empty, true, address(0));
-        
+        bytes32 tempTaskId=IOps(ops).createTaskNoPrepayment(
+            address(this), 
+            this.endGiveaway.selector,
+            address(this),
+            abi.encodeWithSelector(this.checker.selector, newGiveawayNumber),
+            ETH
+        );
+        giveawayMap[newGiveawayNumber] = Giveaway(msg.sender, newGiveawayNumber, _message, _socialLink, _deadline, block.timestamp, msg.value, _participationFee, empty, true, address(0), tempTaskId);
+        emit GiveawayCreated(msg.sender, newGiveawayNumber, _message, _socialLink, _deadline, block.timestamp, msg.value, _participationFee, empty, true, address(0), tempTaskId);
     }
 
     function participate(uint256 giveawayId) public giveawayExist(giveawayId) payable {
         
         Giveaway storage currGiveaway = giveawayMap[giveawayId];
-        uint participationFee = currGiveaway.amount/100;
-        require(participationFee<=msg.value, "Insufficient fee");
-        //Add a check to see if deadline reached and if giveaway has been finished(isLive).
-        //Participant should not participate twice.
+        require(currGiveaway.isLive==true, "Giveaway has been finished");
+        for(uint i=0 ; i<currGiveaway.participants.length ; i++)
+        {
+            require(currGiveaway.participants[i]!=msg.sender, "You cannot participate twice");
+        }
+        require(currGiveaway.participationFee<=msg.value, "Insufficient fee");
         currGiveaway.participants.push(payable(msg.sender));
+        emit GiveawayParticipated(currGiveaway.creator, currGiveaway.uniqueId, currGiveaway.isLive, currGiveaway.amount, currGiveaway.participationFee, currGiveaway.participants);
     }
 
     function getParticipants(uint256 giveawayId) public giveawayExist(giveawayId) view returns (address[] memory) {
-
         return giveawayMap[giveawayId].participants;
-
-    }
+        }
 
     function endGiveaway(uint256 giveawayId) public giveawayExist(giveawayId) {
         require(isLocked==false, "Please try again later");
         Giveaway storage currGiveaway = giveawayMap[giveawayId];
-        require(currGiveaway.creator==msg.sender || msg.sender == owner, "tu koi aur hai"); 
-        // require(currGiveaway.deadline<=block.timestamp, "Deadline not reached");
+        require(currGiveaway.creator==msg.sender || msg.sender == owner || msg.sender == opsAddress, "tu koi aur hai"); 
+        require(currGiveaway.deadline<=block.timestamp, "Deadline not reached");
         require(currGiveaway.isLive==true, "Giveaway already ended");
+        if(currGiveaway.participants.length==0)
+        {
+            payable(currGiveaway.creator).transfer(currGiveaway.amount);
+        }
+
+        uint256 fees;
+        address feeToken;
+        (fees, feeToken) = IOps(ops).getFeeDetails();
+        _transfer(fees);
+        
         currGiveawayId = giveawayId;
         isLocked=true;
         getRandomNumber();
+        emit GiveawayEnded(currGiveaway.creator, currGiveaway.uniqueId, currGiveaway.isLive, currGiveaway.amount, currGiveaway.participationFee, currGiveaway.winner);
     }
 
     modifier giveawayExist(uint256 giveawayId) {
@@ -112,42 +203,167 @@ contract LuckyYou is VRFConsumerBase
     _;
   }
 
-     function getRandomNumber() public returns (bytes32 requestId) {
+     function getRandomNumber() internal returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
         return requestRandomness(keyHash, fee);
     }
 
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness; 
         Giveaway storage currGiveaway = giveawayMap[currGiveawayId];
         uint index = randomness%currGiveaway.participants.length;
         payable(currGiveaway.participants[index]).transfer(currGiveaway.amount);
+        giveawayMap[currGiveawayId].winner=currGiveaway.participants[index];
         giveawayMap[currGiveawayId].isLive=false;
         isLocked=false;
         currGiveawayId=0;
-        giveawayMap[currGiveawayId].winner=currGiveaway.participants[index];
         }
 
-  function random(uint256 giveawayId) private view returns (uint) {
-      Giveaway storage currGiveaway = giveawayMap[giveawayId];
-      return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, currGiveaway.participants)));
-  }
   
   function getAllGiveaways() public view returns (Giveaway[] memory) {
         uint totalGiveaways = giveawayNumber.current();
-        uint giveawayCount = 0;
         uint currIndex = 0;
-        Giveaway[] memory items = new Giveaway[] (giveawayCount);
+        Giveaway[] memory allGiveaways = new Giveaway[] (totalGiveaways);
         for(uint i=0 ; i<totalGiveaways ; i++)
         {
             uint currId = giveawayMap[i+1].uniqueId;
             Giveaway storage currGiveaway = giveawayMap[currId];
-            items[currIndex] = currGiveaway;
+            allGiveaways[currIndex] = currGiveaway;
             currIndex += 1;
         }
-        return items;
+        return allGiveaways;
     }
 
-    //Function changeOwner, unlockContract, 
+   function getParticipatedGiveaways() public view returns (Giveaway[] memory) {
+       uint totalGiveaways = giveawayNumber.current();
+       uint currIndex = 0;
+       uint participatedGiveawaysLength = 0;
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+        for(uint j=0 ; j<currGiveaway.participants.length ; j++)
+        {
+            if(currGiveaway.participants[j]==msg.sender)
+           {
+               participatedGiveawaysLength++;
+
+           }
+
+        }
+       }
+       Giveaway[] memory participatedGiveaways = new Giveaway[] (participatedGiveawaysLength);
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+        for(uint j=0 ; j<currGiveaway.participants.length ; j++)
+        {
+            if(currGiveaway.participants[j]==msg.sender)
+           {
+               participatedGiveaways[currIndex] = currGiveaway;
+               currIndex +=1;
+           }
+
+        }
+       }
+       return participatedGiveaways;
+   }
+
+   function getWonGiveaways() public view returns (Giveaway[] memory) {
+       uint totalGiveaways = giveawayNumber.current();
+       uint currIndex = 0;
+       uint wonGiveawaysLength = 0;
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+           if(currGiveaway.winner==msg.sender && currGiveaway.isLive==false)
+           {
+               wonGiveawaysLength++;
+           }
+       }
+       Giveaway[] memory wonGiveaways = new Giveaway[] (wonGiveawaysLength);
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+           if(currGiveaway.winner==msg.sender && currGiveaway.isLive==false)
+           {
+               wonGiveaways[currIndex] = currGiveaway;
+               currIndex +=1;
+           }
+       }
+
+       return wonGiveaways;
+
+   }
+
+   function getYourGiveaways() public view returns (Giveaway[] memory) {
+       uint totalGiveaways = giveawayNumber.current();
+       uint currIndex = 0;
+       uint yourGiveawaysLength = 0;
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+           if(currGiveaway.creator==msg.sender)
+           {
+               yourGiveawaysLength++;
+           }
+       }
+       Giveaway[] memory yourGiveaways = new Giveaway[] (yourGiveawaysLength);
+       for(uint i=0 ; i<totalGiveaways ; i++)
+       {
+           uint currId = giveawayMap[i+1].uniqueId;
+           Giveaway storage currGiveaway = giveawayMap[currId];
+           if(currGiveaway.creator==msg.sender)
+           {
+               yourGiveaways[currIndex] = currGiveaway;
+               currIndex +=1;
+           }
+       }
+
+       return yourGiveaways;
+
+   }
+
+
+
+   function getTimestamp() public view returns (uint256 time){
+        time = block.timestamp;
+    }
+
+    function rechargeEth() public payable{}
+
+    function checker(uint256 giveawayId)
+        external
+        view
+        returns (bool canExec, bytes memory execPayload)
+    {
+        canExec = block.timestamp >= giveawayMap[giveawayId].deadline && giveawayMap[giveawayId].isLive;
+
+        execPayload = abi.encodeWithSelector(
+            this.endGiveaway.selector,
+            giveawayId
+        );
+    }
+    // getFees(only owner, send fees to owner, only live giveaways in %),
+
+    //Endgiveaway(event), participate(event)
+
+    function changeOwner(address newAddress) public onlyOwner returns (address) {
+        owner = newAddress;
+        return owner;
+    }
+
+    function unlockContract() public onlyOwner {
+        isLocked=false;
+        currGiveawayId=0;
+    }
+
+    // function getFees() onlyOwner {
+    //     payable(owner).transfer()
+    // } 
+
 }
 
